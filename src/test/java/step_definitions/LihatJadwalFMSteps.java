c6765a20b2545c7581da018145fb20ffaf8971b0
AndersenASG <andersenasg88@gmail.com>
2023-06-26 16:19:41 +0700

    add LihatJadwalFM

---
+++ src/test/java/step_definitions/LihatJadwalFMSteps.java
@@ -0,0 +1,52 @@
+package step_definitions;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import org.example.LihatJadwalFMPage;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+
+public class LihatJadwalFMSteps {
+
+    private WebDriver webDriver;
+
+    public LihatJadwalFMSteps(){
+        super();
+        this.webDriver =Hooks.webDriver;
+    }
+
+    @And("User click the find mentor on homepage")
+    public void clickFindMentor () throws InterruptedException {
+        LihatJadwalFMPage lihatJadwalFMPage = new LihatJadwalFMPage(webDriver);
+        lihatJadwalFMPage.clickFindmentor();
+        Thread.sleep(2000);
+    }
+
+    @And("User will direct to search mentor page")
+    public void verifyLandingSearchMentor () throws InterruptedException {
+        LihatJadwalFMPage lihatJadwalFMPage = new LihatJadwalFMPage(webDriver);
+        Assert.assertTrue(lihatJadwalFMPage.verifyLandingSearchMentorPage());
+        Thread.sleep(2000);
+    }
+
+    @And("User click Johannes Hoho Mui mentor")
+    public void clickMentoronSearchPage (String mentor) throws InterruptedException {
+        LihatJadwalFMPage lihatJadwalFMPage = new LihatJadwalFMPage(webDriver);
+        lihatJadwalFMPage.clickMentorJohannes();
+        Thread.sleep(2000);
+    }
+
+    @Then("User will direct to profile detail mentor page")
+    public void verifyLandingMentorDetailPage () throws InterruptedException {
+        LihatJadwalFMPage lihatJadwalFMPage = new LihatJadwalFMPage(webDriver);
+        Assert.assertTrue(lihatJadwalFMPage.verifyLandingMentorDetail());
+        Thread.sleep(2000);
+    }
+
+    @And("User click the Lihat Jadwal Dropdown")
+    public void clickJadwalMarker () throws InterruptedException {
+        LihatJadwalFMPage lihatJadwalFMPage = new LihatJadwalFMPage(webDriver);
+        lihatJadwalFMPage.clickJadwalMarker();
+        Thread.sleep(2000);
+    }
+}
